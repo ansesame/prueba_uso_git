2f5f4106a4a1fbf6071831bbaaccf270c5b068e1
ansesame <ansesame@gmail.com>
2020-05-29 10:07:27 -0500

    Cambios en descripción comandos git

--- hello2.py
+++ hello2.py
@@ -1,1 +1,13 @@
+print("Documento prueba git")
 print("Hola Mundo")
+
+
+#Comandos
+print("git status: mostrar entorno")
+print("git add [nombre archiv])")
+print("git commit: guardado.")
+print("Se debe tener cuidadoen vim, teclear \n",
+      "teclear i para descripción y wq o wq! para confirmar")
+print(" git checkout -- [nombre]: devolver último cambio")
+print("git diff [nombre]: diferencias en cambios")
+print("con +  muestra lo agregado y - lo eliminado")
