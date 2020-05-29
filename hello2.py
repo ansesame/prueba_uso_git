6f8e4d4e78bdd218b52e37821fe4d2556cf478be
ansesame <ansesame@gmail.com>
2020-05-29 10:24:44 -0500

    Comentario commit

--- hello2.py
+++ hello2.py
@@ -8,6 +8,8 @@ print("git add [nombre archiv])")
 print("git commit: guardado.")
 print("Se debe tener cuidadoen vim, teclear \n",
       "teclear i para descripción y wq o wq! para confirmar")
+
+print("git commit -m 'mensaje': evita entrar a vim")
 print(" git checkout -- [nombre]: devolver último cambio")
 print("git diff [nombre]: diferencias en cambios")
 print("con +  muestra lo agregado y - lo eliminado")
